e98e521791bf02a8607f74199dcd171b8cdfc495
Barry Becker <barrybecker4@gmail.com>
2019-12-15 07:12:05 -0800

    Initial working revision using webpack and node.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # graph-viz
 Experiment with graph visualization using G6/antvis.
+
+## How to Build
+npm ci
+npm run build  (uses webpack to build dist/dist.js from sources)
+Open dist/index.html in a browser
---
+++ src/graph-viz.js
@@ -0,0 +1,70 @@
+import G6 from '@antv/g6';
+
+const graph = new G6.Graph({
+    container: 'mountNode',
+    width: 800,
+    height: 600,
+    defaultNode: {
+      size: 30,
+      labelCfg: {
+        style: {
+          fill: '#fff'
+        }
+      }
+    },
+    defaultEdge: {
+      labelCfg: {
+        autoRotate: true
+      }
+    },
+    layout: {
+      type: 'force',            // Force layout
+      linkDistance: 100,        // The link distance is 100
+      preventOverlap: true,     // Prevent node overlappings
+    }
+  });
+  const main = async () => {
+    const response = await fetch(
+      'https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json'
+    );
+    const remoteData = await response.json();
+
+    const nodes = remoteData.nodes;
+    const edges = remoteData.edges;
+    nodes.forEach(node => {
+      if (!node.style) {
+        node.style = {};
+      }
+      node.style.lineWidth = 1;
+      node.style.stroke = '#666';
+      node.style.fill = 'steelblue';
+      switch (node.class) {
+        case 'c0': {
+          node.shape = 'circle';
+          break;
+        }
+        case 'c1': {
+          node.shape = 'rect';
+          node.size = [ 35, 20 ];
+          break;
+        }
+        case 'c2': {
+          node.shape = 'ellipse';
+          node.size = [ 35, 20 ];
+          break;
+        }
+      }
+    });
+    edges.forEach(edge => {
+      if (!edge.style) {
+        edge.style = {};
+      }
+      edge.style.lineWidth = edge.weight;
+      edge.style.opacity = 0.6;
+      edge.style.stroke = 'grey';
+    });
+
+    graph.data(remoteData);
+    graph.render();
+  };
+  main();
